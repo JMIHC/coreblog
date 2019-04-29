ca5a322730bafc2944e415e9991de2e93078a3aa
jmihc <JMIHC@users.noreply.github.com>
2019-04-28 22:02:14 -0500

    adding axios yay! plus more form update

--- src/components/form.js
+++ src/components/form.js
@@ -1,28 +1,78 @@
 import React, { Component } from 'react'
+import axios from 'axios'
+
+function encode (data) {
+  return Object.keys(data)
+    .map(key => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
+    .join('&')
+}
 
 export default class Form extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {}
+  }
+
+  handleChange = e => {
+    this.setState({ [e.target.name]: e.target.value })
+  }
+
+  handleSubmit = e => {
+    e.preventDefault()
+    const form = e.target
+    axios('/', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      body: encode({
+        'form-name': form.getAttribute('name'),
+        ...this.state,
+      }),
+    })
+    //   .then(() => navigateTo(form.getAttribute('action')))
+    //   .catch(error => alert(error))
+  }
+
   render () {
     return (
-      <form name="contact" method="POST" data-netlify="true" netlify-honeypot="bot-field">
-        <p>
+      <form
+        name="contact"
+        method="post"
+        //   action="/thanks/"
+        data-netlify="true"
+        data-netlify-honeypot="bot-field"
+        onSubmit={this.handleSubmit}
+      >
+        {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
+        <input type="hidden" name="form-name" value="contact" />
+        <p hidden>
           <label>
-            Name: <input type="text" name="name" />
+            Don’t fill this out: <input name="bot-field" onChange={this.handleChange} />
           </label>
         </p>
         <p>
           <label>
-            Email*: <input type="email" name="email" />
+            Your name:
+            <br />
+            <input type="text" name="name" onChange={this.handleChange} />
           </label>
         </p>
         <p>
           <label>
-            Message: <textarea name="message" />
+            Your email:
+            <br />
+            <input type="email" name="email" onChange={this.handleChange} />
+          </label>
+        </p>
+        <p>
+          <label>
+            Message:
+            <br />
+            <textarea name="message" onChange={this.handleChange} />
           </label>
         </p>
         <p>
           <button type="submit">Send</button>
         </p>
-        <p>*Your email address will not be sold or shared.</p>
       </form>
     )
   }
