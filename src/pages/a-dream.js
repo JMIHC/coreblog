8d198ef0e56fee576c8c1fcb5dd33e7d64cd6f57
jmihc <JMIHC@users.noreply.github.com>
2019-09-30 20:31:04 -0500

    edits

--- src/pages/a-dream.js
+++ src/pages/a-dream.js
@@ -11,36 +11,36 @@ const Vigilance = props => (
     <h1>A Dream</h1>
     <Content>
       <p>
-        I had dream some time ago that involved a couple of guys, myself and a single cell phone. We agreed on a porn clip and one
-        of us proceeded to take a bottle and ejaculate into it. Then he passes it on to the next person and that person finishes
-        and turns to me to pass me the bottle. An empty look in his eyes said this was perfunctory, a matter of course. I took the
-        bottle and then thought wait, where is the cell phone? How am I going to do this without it? And I woke up.
+        I had a dream some time ago that involved a couple of guys, myself and a single cell phone. We agreed on a porn clip and
+        one of us proceeded to take a bottle and ejaculate into it. Then he passes it on to the next person and that person
+        finishes and turns to me to pass me the bottle. An empty look in his eyes said this was perfunctory, a matter of course. I
+        took the bottle and then thought wait, where is the cell phone? How am I going to do this without it? And I woke up.
       </p>
       <p>
         One of parts of the dream that haunts me now is the blank look on his face. Then the dark and dank cave we were in and the
-        way we were expelling our life force. We all knew what was happening, and part of the code amongst us is if you cover for
-        me then I’ll cover for you. We don’t talk about this. You don’t break the circle. Don’t throw off the mantle of misogyny,
+        way we were expelling our life force. We all knew what was happening, and part of the code among us is if you cover for me
+        then I’ll cover for you. We don’t talk about this. You don’t break the circle. Don’t throw off the mantle of misogyny,
         it’s all we’ve got.
       </p>
       <p>
-        There is a lot to unpack here when it comes to how pornography has dented my brain. How I found that it trains my body to
-        use someone instead of engaging in collaborative intimacy. The way those fake images of lust leave imprints that carry
-        well beyond the insecure few minutes of a clip. Life beyond those moments makes objects out of real people. And I don't
-        think I was even really awake after those moments. Judging by the absent gaze I had drifting about and the subtle ways I'd
-        get trapped in the application of pornographic scenes overlaying the normal goings on of life.
+        There is a lot to unpack here when it comes to how pornography dented my brain. How I found that it trains my body to use
+        someone instead of engaging in collaborative intimacy. The way those fake images of lust leave imprints that carry well
+        beyond the insecure few minutes of a clip. Life beyond those moments makes objects out of real people. And I don't think I
+        was even really awake after those moments. Judging by the absent gaze I had drifting about and the subtle ways I'd get
+        trapped in the application of pornographic scenes overlaying the normal goings on of life.
       </p>
       <p>
         Let's consider then, in the midst of hyper-sexualized everything, what to do. So far, I've found that elminating that
         visual stimulus is the first step. But yes, that's tricky. Because it's baked into the brain routines, to varying degrees
         depending on long you've been a symbiotic host to this particular affliction. For me it didn't work to simply stop, there
-        was always a route back. I had to replace it. For me it was gaming. A lot of it. And that has its own detriments when
-        carried to excess.
+        was always a route back. I had to replace it. That was a lot of gaming. And that has its own detriments when carried to
+        excess.
       </p>
       <p>
-        Past that I found it's ultimately about connecting with others in genuine ways. That takes the edge off those grinding
-        gears struggling to adapt to healthier ways of thinking and being. It's also about being vigilant and slowly reclaiming
-        your own body and its creative life force. It is your horse after all, and a far ride to endure if your soul is to
-        complete it's purpose on this earth.
+        I found it's ultimately about connecting with others in genuine ways. That takes the edge off those grinding gears
+        struggling to adapt to healthier ways of thinking and being. It's also about being vigilant and slowly reclaiming your own
+        body and its creative life force. It is your horse after all, and a far ride to endure if your soul is to complete it's
+        purpose on this earth.
       </p>
     </Content>
     <br />
